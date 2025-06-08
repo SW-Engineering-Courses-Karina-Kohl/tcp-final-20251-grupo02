4a5fb5759b2029388da2c4711fb04b468dd43b62
Rayan Raddatz <rayandematos@gmail.com>
2025-06-08 19:31:34 -0300

    identação

--- src/app/Main.java
+++ src/app/Main.java
@@ -27,41 +27,41 @@ public class Main {
         Jogo jogo = new Jogo();
         jogo.NovoJogo(300);
 
-        
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(jogo.getTabuleiro());
 
         while (true) {
             String tBranco = jogo.getJogadorBranco().getRelogio().formatarTempo();
-                String tPreto  = jogo.getJogadorPreto() .getRelogio().formatarTempo();
-                // Só atualiza a linha dos relógios, sem pular linha
-                System.out.println("\rTempo BRANCO: " + tBranco +
-                                 "    Tempo PRETO: " + tPreto);
+	    String tPreto  = jogo.getJogadorPreto() .getRelogio().formatarTempo();
+	    // Só atualiza a linha dos relógios, sem pular linha
+	    System.out.println("\rTempo BRANCO: " + tBranco +
+			       "    Tempo PRETO: " + tPreto);
             System.out.println("Coordenacas da peça e da casa para mover: ");
-			int px = scanner.nextInt() - 1;
-			int py = scanner.nextInt() - 1;
+	    int px = scanner.nextInt() - 1;
+	    int py = scanner.nextInt() - 1;
 
-			int mx = scanner.nextInt() - 1;
-			int my = scanner.nextInt() - 1;
+	    int mx = scanner.nextInt() - 1;
+	    int my = scanner.nextInt() - 1;
 
-			Peca peca = jogo.tabuleiro.GetPecaNaPosicao(px, py);
-			Peca peca2 = jogo.tabuleiro.GetPecaNaPosicao(mx, my);
+	    Peca peca = jogo.tabuleiro.GetPecaNaPosicao(px, py);
+	    Peca peca2 = jogo.tabuleiro.GetPecaNaPosicao(mx, my);
 
-			System.out.println("Peça movida: " + peca.identificador);
-			System.out.println("Peça capturada: " + peca2.identificador);
+	    System.out.println("Peça movida: " + peca.identificador);
+	    System.out.println("Peça capturada: " + peca2.identificador);
 
-			Jogada jogada = new Jogada(peca, peca2);
-			
-			if( jogada.ValidarJogada(jogo.tabuleiro) ){
-				jogo.tabuleiro.MudancaNoTabuleiro(jogada);
-				jogada.peca_movida.Mover(jogada);
-				jogada.peca_capturada.DestruirPeca();
+	    Jogada jogada = new Jogada(peca, peca2);
+
+	    if( jogada.ValidarJogada(jogo.tabuleiro) ){
+		jogo.tabuleiro.MudancaNoTabuleiro(jogada);
+		jogada.peca_movida.Mover(jogada);
+		jogada.peca_capturada.DestruirPeca();
 
                 jogo.ProximoTurno();
-			} 
+	    }
 
-			System.out.println(jogo.tabuleiro);
+	    System.out.println(jogo.tabuleiro);
         }
     }
 }
