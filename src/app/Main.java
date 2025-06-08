58b3e4108b89a449cfca629e531deb0c122c392e
Rayan Raddatz <rayandematos@gmail.com>
2025-06-08 20:14:49 -0300

    comentarios para o Iuri

--- src/app/Main.java
+++ src/app/Main.java
@@ -24,6 +24,8 @@ public class Main {
         //     EndDrawing();
         // }
         // CloseWindow();
+
+	// Cria um novo jogo
         Jogo jogo = new Jogo();
         jogo.NovoJogo(300);
 
@@ -34,31 +36,40 @@ public class Main {
 
         while (true) {
             String tBranco = jogo.getJogadorBranco().getRelogio().formatarTempo();
-	    String tPreto  = jogo.getJogadorPreto() .getRelogio().formatarTempo();
+	    String tPreto  = jogo.getJogadorPreto().getRelogio().formatarTempo();
 	    // Só atualiza a linha dos relógios, sem pular linha
 	    System.out.println("\rTempo BRANCO: " + tBranco +
 			       "    Tempo PRETO: " + tPreto);
             System.out.println("Coordenacas da peça e da casa para mover: ");
+
+	    // Le as peças do teclado
 	    int px = scanner.nextInt() - 1;
 	    int py = scanner.nextInt() - 1;
 
 	    int mx = scanner.nextInt() - 1;
 	    int my = scanner.nextInt() - 1;
 
+	    // pega as peças nas posições lidas
 	    Peca peca = jogo.tabuleiro.GetPecaNaPosicao(px, py);
 	    Peca peca2 = jogo.tabuleiro.GetPecaNaPosicao(mx, my);
 
 	    System.out.println("Peça movida: " + peca.identificador);
 	    System.out.println("Peça capturada: " + peca2.identificador);
 
+	    // Cria uma jogada com as peças
 	    Jogada jogada = new Jogada(peca, peca2);
 
+	    // Válida a jogada
 	    if( jogada.ValidarJogada(jogo.tabuleiro) ){
+
+		// Se for válida, muda o tabuleiro
 		jogo.tabuleiro.MudancaNoTabuleiro(jogada);
+
+		// Atualiza as peças
 		jogada.peca_movida.Mover(jogada);
 		jogada.peca_capturada.DestruirPeca();
 
-                jogo.ProximoTurno();
+                jogo.ProximoTurno(); // atualiza o turno
 	    }
 
 	    System.out.println(jogo.tabuleiro);
