432bfc5d7a4e8256f48b1cf44cdefbdc1194c6d9
Gabriel Henrique Fiszczuk Brandeburski <brandeburskigabrielhenrique@gmail.com>
2025-06-21 18:00:15 -0300

    Some tests for the Move and Pieces classes

--- test/game/pieces/PiecesTest.java
+++ test/game/pieces/PiecesTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import game.Move;
 
-import misc.Pair;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PiecesTest {
@@ -19,12 +17,12 @@ public class PiecesTest {
 
     @Test
     public void testFindPieceColorBlack() {
-        Piece blackBishop = new Bishop(2, 0, 'b');
-        Piece blackKing = new King(4, 0, 'r');
-        Piece blackKnight = new Knight(1, 0, 'c');
-        Piece blackPawn = new Pawn(0, 1, 'p');
-        Piece blackQueen = new Queen(3, 0, 'd');
-        Piece blackRook = new Rook(0, 0, 't');
+        Piece blackBishop = new Bishop(2, 0, 'b', false);
+        Piece blackKing = new King(4, 0, 'r', false);
+        Piece blackKnight = new Knight(1, 0, 'c', false);
+        Piece blackPawn = new Pawn(0, 1, 'p', false);
+        Piece blackQueen = new Queen(3, 0, 'd', false);
+        Piece blackRook = new Rook(0, 0, 't', false);
 
         assertEquals(blackBishop.findPieceColor(), 'b');
         assertEquals(blackKing.findPieceColor(), 'b');
@@ -37,12 +35,12 @@ public class PiecesTest {
 
     @Test
     public void testFindPieceColorWhite() {
-        Piece whiteBishop = new Bishop(2, 7, 'B');
-        Piece whiteKing = new King(4, 7, 'R');
-        Piece whiteKnight = new Knight(1, 7, 'C');
-        Piece whitePawn = new Pawn(0, 6, 'P');
-        Piece whiteQueen = new Queen(3, 7, 'D');
-        Piece whiteRook = new Rook(0, 7, 'T');
+        Piece whiteBishop = new Bishop(2, 7, 'B', false);
+        Piece whiteKing = new King(4, 7, 'R', false);
+        Piece whiteKnight = new Knight(1, 7, 'C', false);
+        Piece whitePawn = new Pawn(0, 6, 'P', false);
+        Piece whiteQueen = new Queen(3, 7, 'D', false);
+        Piece whiteRook = new Rook(0, 7, 'T', false);
 
         assertEquals(whiteBishop.findPieceColor(), 'w');
         assertEquals(whiteKing.findPieceColor(), 'w');
@@ -55,10 +53,12 @@ public class PiecesTest {
 
     @Test
     public void testMovePiece(){
-        Piece whiteQueen = new Queen(3, 7, 'D');
+        Piece whiteQueen = new Queen(3, 7, 'D', false);
         Piece blank = new Blank(7,7);
         Move move = new Move(whiteQueen,blank);
         whiteQueen.movePiece(move);
-        assertEquals(new Pair(7, 7), whiteQueen.getBoardPosition());
+        assertEquals(7, whiteQueen.getBoardPosition().x);
+        assertEquals(7, whiteQueen.getBoardPosition().y);
+
     } 
 }
